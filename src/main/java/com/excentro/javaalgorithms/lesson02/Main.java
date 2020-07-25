76ad1750ed4fdd2e19c28ed37d1c887cde233d04
Mikhail Mishin <excentro@gmail.com>
2020-07-25 09:58:14 +0300

    Пытаюсь сделать copy constructor

--- src/main/java/com/excentro/javaalgorithms/lesson02/Main.java
+++ src/main/java/com/excentro/javaalgorithms/lesson02/Main.java
@@ -1,7 +1,46 @@
 package com.excentro.javaalgorithms.lesson02;
 
-public class Main {
-  public static void main(String[] args) {
+import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * 1. Создать массив большого размера (миллион элементов). 2. Заполнить массив случайными числами.
+ * 3. Проверить скорость выполнения каждой сортировки. (Если время сортировки будет не приемлимо
+ * долго, можете для массива из 100 000 элементов проверить)
+ *
+ * Пузырьковая сортировка 35910 мс
+ *
+ * Сортировка выбором 5890 мс
+ *
+ * Сортировка вставками 12827 мс
+ */
+public class Main {
+  /** Main class. */
+  public static void main(final String[] args) {
+    // Создаем массив
+    final MyArrayList<Integer> arr = new MyArrayList<>();
+    for (int i = 0; i < 100_000; i++) {
+      arr.add(ThreadLocalRandom.current().nextInt(100));
+    }
+    final MyArrayList<Integer> arr2 = MyArrayList(arr);
+    final MyArrayList<Integer> arr3 = MyArrayList(arr);
+
+    long startTime = System.currentTimeMillis();
+    arr.bubbleSort();
+    long stopTime = System.currentTimeMillis();
+    long elapsedTime = stopTime - startTime;
+    System.out.println("BubbleSort time: " + elapsedTime);
+
+    startTime = System.currentTimeMillis();
+    arr2.selectionSort();
+    stopTime = System.currentTimeMillis();
+    elapsedTime = stopTime - startTime;
+    System.out.println("SelectionSort time: " + elapsedTime);
+
+    startTime = System.currentTimeMillis();
+    arr3.insertionSort();
+    stopTime = System.currentTimeMillis();
+    elapsedTime = stopTime - startTime;
+    System.out.println("InsertionSort time: " + elapsedTime);
   }
+
 }
--- src/main/java/com/excentro/javaalgorithms/lesson02/MyArrayList.java
+++ src/main/java/com/excentro/javaalgorithms/lesson02/MyArrayList.java
@@ -3,41 +3,63 @@ package com.excentro.javaalgorithms.lesson02;
 import java.util.Comparator;
 
 public class MyArrayList<T extends Comparable<T>> {
-  private static final int DEFAULT_CAPACITY = 10;
+  private static final int DEFAULT_CAPACITY = 100_000;
   private final T[] list;
   private int size = 0;
 
-  public MyArrayList(int capacity) {
+  /**
+   * Конструктор.
+   */
+  public MyArrayList(final int capacity) {
     if (capacity < 0) {
       throw new IllegalArgumentException("capacity : " + capacity);
     }
     list = (T[]) new Comparable[capacity];
   }
 
+  /** Copy constructor. */
+  public MyArrayList(MyArrayList<T> arr) {
+    list = arr.list;
+  }
+
   public MyArrayList() {
     list = (T[]) new Comparable[DEFAULT_CAPACITY];
   }
 
-  public void add(T item) {
+  public void add(final T item) {
     list[size] = item;
     size++;
   }
 
-  public void add(int index, T item) {
+
+  /**
+   * Добавляет элемент.
+   *
+   * @param index позиция
+   * @param item  эелемент
+   */
+  public void add(final int index, final T item) {
     checkCorrectIndex(index);
 
-    if (size - index >= 0) System.arraycopy(list, index, list, index + 1, size - index);
+    if (size - index >= 0) {
+      System.arraycopy(list, index, list, index + 1, size - index);
+    }
     list[index] = item;
     size++;
   }
 
-  private void checkCorrectIndex(int index) {
+  private void checkCorrectIndex(final int index) {
     if (index < 0 || index > size) {
       throw new IllegalArgumentException("no correct index" + index);
     }
   }
 
-  public boolean remove(T item) {
+  /**
+   * Удаляем элемент.
+   *
+   * @param item элемент
+   */
+  public boolean remove(final T item) {
     int i = 0;
     while (i < size && !list[i].equals(item)) {
       i++;
@@ -45,18 +67,20 @@ public class MyArrayList<T extends Comparable<T>> {
     if (i == size) {
       return false;
     }
-    if (size - 1 - i >= 0) System.arraycopy(list, i + 1, list, i, size - 1 - i);
+    if (size - 1 - i >= 0) {
+      System.arraycopy(list, i + 1, list, i, size - 1 - i);
+    }
     size--;
     list[size] = null;
     return true;
   }
 
-  public T get(int index) {
+  public T get(final int index) {
     checkCorrectIndex(index);
     return list[index];
   }
 
-  public void set(int index, T item) {
+  public void set(final int index, final T item) {
     checkCorrectIndex(index);
     list[index] = item;
   }
@@ -65,11 +89,17 @@ public class MyArrayList<T extends Comparable<T>> {
     return size;
   }
 
-  public boolean contains(T item) {
+  public boolean contains(final T item) {
     return indexOf(item) > -1;
   }
 
-  public final int indexOf(T item) {
+  /**
+   * Находит индекс элемента.
+   *
+   * @param item искомый элемент
+   * @return позиция элемента
+   */
+  public final int indexOf(final T item) {
     for (int i = 0; i < size; i++) {
       if (list[i].equals(item)) {
         return i;
@@ -80,7 +110,7 @@ public class MyArrayList<T extends Comparable<T>> {
 
   @Override
   public String toString() {
-    StringBuilder sb = new StringBuilder("[ ");
+    final StringBuilder sb = new StringBuilder("[ ");
     for (int i = 0; i < size; i++) {
       sb.append(list[i]).append(", ");
     }
@@ -89,28 +119,34 @@ public class MyArrayList<T extends Comparable<T>> {
     return sb.toString();
   }
 
+  /**
+   * Сортировка выбором.
+   */
   public void selectionSort() {
     for (int i = 0; i < size - 1; i++) {
-      int iMin = i;
+      int nimElem = i;
       for (int j = i + 1; j < size; j++) {
-        if (less(list[j], list[iMin])) {
-          iMin = j;
+        if (less(list[j], list[nimElem])) {
+          nimElem = j;
         }
       }
-      swap(i, iMin);
+      swap(i, nimElem);
     }
   }
 
-  private boolean less(T item1, T item2) {
+  private boolean less(final T item1, final T item2) {
     return item1.compareTo(item2) < 0;
   }
 
-  private void swap(int index1, int index2) {
-    T temp = list[index1];
+  private void swap(final int index1, final int index2) {
+    final T temp = list[index1];
     list[index1] = list[index2];
     list[index2] = temp;
   }
 
+  /**
+   * Сортировка вставками.
+   */
   public void insertionSort() {
     T key;
     for (int i = 1; i < size; i++) {
@@ -124,6 +160,7 @@ public class MyArrayList<T extends Comparable<T>> {
     }
   }
 
+  /** Пузырьковая сортировка. */
   public void bubbleSort() {
     boolean isSwapped;
     for (int i = size - 1; i > 0; i--) {
@@ -140,7 +177,12 @@ public class MyArrayList<T extends Comparable<T>> {
     }
   }
 
-  public void bubbleSort(Comparator<T> comparator) {
+  /**
+   * Пузырьковая сортировка с компаратором.
+   *
+   * @param comparator компаратор
+   */
+  public void bubbleSort(final Comparator<T> comparator) {
     boolean isSwapped;
     for (int i = size - 1; i > 0; i--) {
       isSwapped = false;
