ae0271b1f2270de30b64b9e590fe7d1c7eea5712
Mikhail Mishin <excentro@gmail.com>
2020-07-28 19:47:50 +0300

    1-й пункт 3-го дз

--- src/main/java/com/excentro/javaalgorithms/lesson03/Main.java
+++ src/main/java/com/excentro/javaalgorithms/lesson03/Main.java
@@ -2,7 +2,21 @@ package com.excentro.javaalgorithms.lesson03;
 
 import java.util.Scanner;
 
-/** 1. Создать программу, которая переворачивает вводимые строки (читает справа налево). */
 public class Main {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    System.out.println("Enter a string: ");
+    invertString(reader.nextLine());
+  }
 
+  /** 1. Создать программу, которая переворачивает вводимые строки (читает справа налево). */
+  private static void invertString(String reader) {
+    MyStack<Character> stack = new MyStack<>(reader.length());
+    for (int i = 0; i < reader.length(); i++) {
+      stack.push(reader.charAt(i));
+    }
+    while (!stack.isEmpty()) {
+      System.out.print(stack.pop());
+    }
+  }
 }
