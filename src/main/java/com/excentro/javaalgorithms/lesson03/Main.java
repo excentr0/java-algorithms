0665f96f3fd279e77715c8bc1fedec911ce284a3
Mikhail Mishin <excentro@gmail.com>
2020-07-28 21:46:41 +0300

    Сделал 3й пункт 3-го ДЗ

--- src/main/java/com/excentro/javaalgorithms/lesson03/Main.java
+++ src/main/java/com/excentro/javaalgorithms/lesson03/Main.java
@@ -1,5 +1,6 @@
 package com.excentro.javaalgorithms.lesson03;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -7,25 +8,24 @@ public class Main {
     reverseString();
 
     testDeque();
+
+    priorityQueueWithComparator();
+  }
+
+  private static void priorityQueueWithComparator() {
+    MyPriorityQueue<Integer> queue = new MyPriorityQueue<>(10, Comparator.naturalOrder());
+    queue.insertComp(1);
+    queue.insertComp(2);
+    queue.insertComp(3);
+    queue.insertComp(4);
+    queue.insertComp(5);
+    System.out.println(queue);
   }
 
   private static void reverseString() {
     Scanner reader = new Scanner(System.in);
     System.out.println("Enter a string: ");
     invertString(reader.nextLine());
-  }
-
-  private static void testDeque() {
-    MyDeque<Integer> deque = new MyDeque<>();
-    deque.insertLeft(1);
-    deque.insertLeft(2);
-    deque.insertLeft(3);
-    deque.insertRight(4);
-    deque.insertRight(5);
-    deque.insertRight(6);
-    deque.removeLeft();
-    deque.removeRight();
-    System.out.println(deque);
   }
 
   /** 1. Создать программу, которая переворачивает вводимые строки (читает справа налево). */
@@ -37,5 +37,18 @@ public class Main {
     while (!stack.isEmpty()) {
       System.out.print(stack.pop());
     }
+  }
+
+  private static void testDeque() {
+    MyDeque<Integer> deque = new MyDeque<>();
+    deque.insertLeft(3);
+    deque.insertLeft(2);
+    deque.insertLeft(1);
+    deque.insertRight(4);
+    deque.insertRight(5);
+    deque.insertRight(6);
+    deque.removeLeft();
+    deque.removeRight();
+    System.out.println(deque);
   }
 }
