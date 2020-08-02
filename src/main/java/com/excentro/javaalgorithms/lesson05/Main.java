f1feb971abd98fad4619c90b5b5ba2af84235b84
Mikhail Mishin <excentro@gmail.com>
2020-08-02 17:01:55 +0300

    1. Написать программу по возведению числа в степень с помощью рекурсии.

--- src/main/java/com/excentro/javaalgorithms/lesson05/Main.java
+++ src/main/java/com/excentro/javaalgorithms/lesson05/Main.java
@@ -2,6 +2,31 @@ package com.excentro.javaalgorithms.lesson05;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    System.out.println(power(2, 5));
+    System.out.println(fastPower(2, 5));
   }
+
+  /** Возведение в степень. */
+  public static long power(long x, long n) {
+    if (n == 1) {
+      return x;
+    } else {
+      return x * power(x, n - 1);
+    }
+  }
+
+  /** Быстрое возведение в степень. */
+  public static long fastPower(long x, long n) {
+    if (n == 0) {
+      return 1;
+    }
+    if (n % 2 == 1) {
+      return fastPower(x, n - 1) * x;
+    } else {
+      long p = fastPower(x, n / 2);
+      return p * p;
+    }
+
+  }
+
 }
