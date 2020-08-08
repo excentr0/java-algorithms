2fad31bd4059886176cd40a13fe3c9e670e23bee
Mikhail Mishin <excentro@gmail.com>
2020-08-08 20:24:02 +0300

    fix

--- src/main/java/com/excentro/javaalgorithms/lesson06/Main.java
+++ src/main/java/com/excentro/javaalgorithms/lesson06/Main.java
@@ -28,8 +28,5 @@ public class Main {
     }
     System.out.println("Процент несбалансированных деревьев: " + (float) balanced / count * 100);
 
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello");
   }
 }
