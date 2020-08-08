9f337b20395893c16502be41095fcd55b92b3f53
Mikhail Mishin <excentro@gmail.com>
2020-08-08 09:52:05 +0300

    Изменил генерацию деревьев. Исправил результат задания

--- src/main/java/com/excentro/javaalgorithms/lesson06/Main.java
+++ src/main/java/com/excentro/javaalgorithms/lesson06/Main.java
@@ -10,13 +10,15 @@ public class Main {
    * случайным образом в диапазоне от -100 до 100.
    *
    * 2. Проанализировать, какой процент созданных деревьев являются несбалансированными.
+   *
+   * Процент несбалансированных деревьев: 85.0 - 90.0
    */
   public static void main(String[] args) {
     int balanced = 0;
     int count = 20;
     for (int i = 0; i < count; i++) {
       MyTreeMap<Integer, Integer> treeMap = new MyTreeMap<>();
-      int level = ThreadLocalRandom.current().nextInt(6, 64);
+      int level = ThreadLocalRandom.current().nextInt(64);
       for (int j = 0; j < level; j++) {
         int randomNum = ThreadLocalRandom.current().nextInt(-100, 100);
         treeMap.put(randomNum, randomNum);
